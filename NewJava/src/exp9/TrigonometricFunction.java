e55fc9723823067aefcd7cfe7e7e838e9b79f87f
yang837 <1535270292@qq.com>
2022-05-23 16:41:05 +0800

    Create TrigonometricFunction.java

---
+++ NewJava/src/exp9/TrigonometricFunction.java
@@ -0,0 +1,9 @@
+package exp9;
+
+public class TrigonometricFunction {
+    public static void main(String[] args) {
+        System.out.println(Math.sin(Math.PI / 2));
+        System.out.println(Math.toRadians(120));
+        System.out.println(Math.toDegrees(Math.PI));
+    }
+}
