5b75eeb39665b68dea6f47013fc3180b9255b7df
yang837 <1535270292@qq.com>
2022-06-21 10:43:03 +0800

    Update Solution.java

--- NewJava/src/leetcode/Solution.java
+++ NewJava/src/leetcode/Solution.java
@@ -62,6 +62,23 @@ class Solution {
 
 
 
+    public int coinChange(int[] coins, int amount) {
+        int max = amount + 1;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, max);
+
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (i - coins[j] >= 0) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
+            }
+        }
+
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+
     public int uniquePaths(int m, int n) {
         // dp[m - 1][n - 1]为m*n网格从左上角到右下角位置路径数
         int[][] dp = new int[m][n];
