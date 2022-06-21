673f002e65b6ee7b8f9d679d21d1bbd671783e47
yang837 <1535270292@qq.com>
2022-06-21 10:43:08 +0800

    Update Solution.java

--- NewJava/src/Solution.java
+++ NewJava/src/Solution.java
@@ -70,21 +70,10 @@ class Node {
 
 class Solution {
 
-    public int coinChange(int[] coins, int amount) {
-        int max = amount + 1;
-        int[] dp = new int[amount + 1];
-        Arrays.fill(dp, max);
+    public int lengthOfLIS(int[] nums) {
+        
 
-        dp[0] = 0;
-        for (int i = 1; i <= amount; i++) {
-            for (int j = 0; j < coins.length; j++) {
-                if (i - coins[j] >= 0) {
-                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
-                }
-            }
-        }
-
-        return dp[amount] > amount ? -1 : dp[amount];
+        return 0;
     }
 
     public void swap(int[] a, int i, int j) {
